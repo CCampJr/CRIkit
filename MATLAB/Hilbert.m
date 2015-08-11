84e8947022dd045e0abde45c7ff54f3601315243
Charles H Camp Jr <ccampjr@gmail.com>
2015-08-11 17:03:02 -0400

    Added MATLAB files for pre-processing.
    Signed-off-by: Charles H Camp Jr <ccampjr@gmail.com>

---
+++ MATLAB/Hilbert.m
@@ -0,0 +1,77 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%
+%%%     Hilbert - FFT implementation of the Hilbert transform that takes
+%%%     in a signal (or multiple signals in parallel) and outputs an
+%%%     analytic signal(s) based on the Hilbert transform.
+%%%
+%%%     If you use this software, please cite our work (see below).
+%%%
+%%%     Analytic = Hilbert(Input,padfactor)
+%%%
+%%%     Inputs:
+%%%         Input - input signal(s) (M x 1; 1 x M; M x N) -- see note on
+%%%             parallel processing
+%%%         padfactor (optional; default = 1) - (0 or positive integer) multiplier of the original
+%%%             signal size M for each padding region. E.g., M = 1000,
+%%%             padfactor = X, length of signal during processing X*M+M+X*M =
+%%%             2X*M+M. Output signal length = M.
+%%%     Output:
+%%%         Analytic - analytical representation of input signal. NOTE: real(Analytic) =
+%%%         Input. imag(Analytic) = Hilbert{Input}
+%%%
+%%%     NOTE ON PARALLEL PROCESSING - This function can process N spectra
+%%%     simultaneously. Make sure the input signal is M x N, with M =
+%%%     signal length, N = # of signals
+%%%
+%%%     CITATION: C. H. Camp Jr., Y. J. Lee, and M. T. Cicerone, 
+%%%         "Quantitative, Comparable Coherent Anti-Stokes Raman Scattering
+%%%         (CARS) Spectroscopy: Correcting Errors in Phase Retrieval,"
+%%%         (2015). arXiv:1507.06543.
+%%%
+%%%     Charles H Camp Jr (charles.camp@nist.gov, ccampjr@gmail.com)
+%%%         v0: 9/11/2014
+%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function Analytic = Hilbert(Input,varargin)
+
+% Was padfactor given?
+if length(varargin) == 1
+    pad_factor = varargin{1};
+else
+    pad_factor = 1;
+end
+
+% Ensure a 1D signal is M x 1; else make it M x 1.
+[m,n] = size(Input);
+if (m == 1 | n == 1)
+    Input = Input(:);
+else
+    ;
+end
+[m,n] = size(Input);
+
+% Add padfactor*M padding. Before -- padfactor*M @ Amp = Input(1); After -- padfactor*M @ Amp = Input(end);
+pad_low = ones(pad_factor*m,1)*Input(1,:);
+pad_high = ones(pad_factor*m,1)*Input(end,:);
+
+Input_padded = [pad_low;Input;pad_high];
+[m2,n2] = size(Input_padded);
+
+% Operate in Fourier-domain
+Input_fft_padded = fft(Input_padded);
+t_temp = linspace(1,-1,m2).';
+Mask = j.*sign(t_temp); % Represents Hilbert as an amplitude mask in the Fourier-domain
+%Transform_mask = Mask*ones(1,n2);
+Transform_mask = t_temp*ones(1,n2);
+Analytic_fft_padded = Transform_mask.*Input_fft_padded;
+
+% iFFT
+%Analytic_padded = real(ifft(Analytic_fft_padded));
+Analytic_padded = ifft(Analytic_fft_padded);
+
+% De-pad and output analytic signal
+%Analytic = Input + j.*(Analytic_padded(m*pad_factor+1:m*pad_factor+m,:));
+Analytic = Input + j.*imag(Analytic_padded(m*pad_factor+1:m*pad_factor+m,:));
+
+
---
+++ MATLAB/KKHilbert.m
@@ -0,0 +1,114 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%
+%%%     KKHilbert - Retrieve real and imaginary components of raw CARS
+%%%     spectrum utilizing a Kramers-Kronig relation. 
+%%%     
+%%%     This is a re-implementation of the "modified time-domain 
+%%%     Kramers-Kronig transform" (see References) that is computationally
+%%%     more efficient and utilizes the Hilbert transform explicitly. If
+%%%     you use this software please cite our work (see below).
+%%%
+%%%     Required additional functions: MyHilbert.m
+%%%
+%%%     [KK_Imag,KK_Real] = KKHilbert(NRB,CARS,[PhaseOffset],[NRBNorm])
+%%%
+%%%     Inputs:
+%%%         NRB - Nonresonant background signal(s). (M x 1; 1 x M; or M x N) 
+%%%             See note on parallel computations
+%%%         CARS - Raw BCARS signal(s). (M x 1; 1 x M; or M x N). 
+%%%             See note on parallel computations.
+%%%         PhaseOffset (optional, default = 0)- (rad). (1 x 1; M x 1; 1 x M; or M x N).
+%%%             Constant or vector phase to add to retrieved phase of KKd and KKdR
+%%%         NRBNorm (optional, default = 1 [Yes]) - (1 x 1). Normalize 
+%%%             Retrieved spectrum to the NRB: 1 = yes, 0 = No.
+%%%
+%%%         NOTE ON OPTIONAL PARAMETERS - Neither or both optional
+%%%             parameters must be provided.
+%%%
+%%%     Outputs:
+%%%         KK_Imag - imaginary component of input signal(s). (M x 1, or M x N)
+%%%         KK_Real - real component of input signal(s). (M x 1, or M x N)
+%%%
+%%%     NOTE ON PARALLEL PROCESSING - This function can process N spectra
+%%%     simultaneously. Make sure the input signal is M x N, with M =
+%%%     signal length, N = # of signals
+%%%
+%%%     CITATION: C. H. Camp Jr., Y. J. Lee, and M. T. Cicerone, 
+%%%         "Quantitative, Comparable Coherent Anti-Stokes Raman Scattering
+%%%         (CARS) Spectroscopy: Correcting Errors in Phase Retrieval,"
+%%%         (2015). arXiv:1507.06543.
+%%%
+%%%     REFERENCE: Y. Liu, Y. J. Lee, and M. T. Cicerone, 
+%%%         "Broadband CARS spectral phase retrieval using a time-domain 
+%%%         Kramers-Kronig transform," Optics Letters 34, 1363-1365 (2009).
+%%%
+%%%     Charles H Camp Jr (charles.camp@nist.gov, ccampjr@gmail.com) 
+%%%         v2_0: 9/11/2014
+%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function [KK_Imag,KK_Real] = KKHilbert(NRB,CARS,PhaseOffset,NRBNorm)
+
+% Are optional parameters supplied?
+if nargin == 2 % No. Set defaults
+    PhaseOffset = 0;
+    NRBNorm = 1;
+end
+
+% Ensure 1D PhaseOfsset size is M x 1
+if(size(PhaseOffset,1) == 1 | size(PhaseOffset,2) == 1)
+    PhaseOffset = PhaseOffset(:);
+else
+    ;
+end
+
+[m,n] = size(CARS);
+[m2,n2] = size(NRB);
+
+% If CARS is 1D, ensure M x 1 size
+if m == 1 & n > 1
+    CARS = CARS.';
+    [m,n] = size(CARS);
+end
+
+% If NRB is 1D, ensure M x 1 size
+if m2 == 1 & n2 > 1
+    NRB = NRB.';
+    [m2,n2] = size(NRB);
+end
+
+% Make sure NRB and CARS spectral length are the same
+if m~= m2
+    errordlg(['Incorrect Spectral Axis Size. NRB: ' num2str(m2) ' CARS: ' num2str(m)])
+    error(['Incorrect Spectral Axis Size. NRB: ' num2str(m2) ' CARS: ' num2str(m)])
+end
+
+% For N > 1 signals, make sure NRB has either 1 spectrum (applied across
+% all CARS spectra) or N spectra (N = # CARS spectra)
+if (n2 > 1 & n2 ~= n)
+    errordlg(['Incorrect # of Spectra. NRB: ' num2str(n2) ' CARS: ' num2str(n)])
+    error(['Incorrect # of Spectra. NRB: ' num2str(n2) ' CARS: ' num2str(n)])
+elseif (n2 == 1 & n > 1) % If CARS = N spectra and NRB = 1 spectrum; Make N replicates of NRB
+    NRB = NRB*ones(1,n);
+end
+
+temp_in = CARS./NRB;
+
+% Find and replace NaN and Inf values with 1 (log(1) = 0). Alterantive, can
+% replace 1 with a small value, but may greatly increase noise.
+vec = find(isnan(temp_in)==1);
+temp_in(vec) = 1;
+vec = find(isinf(temp_in)==1);
+temp_in(vec) = 1;
+vec = find(temp_in==0);
+temp_in(vec) = 1;
+
+
+if NRBNorm == 1 % Normalize to NRB -- (Output = sqrt(CARS/NRB)*exp(j*phi))
+    temp = real(sqrt(temp_in)).*exp(j.*PhaseOffset+j.*imag(Hilbert(.5.*real(log(temp_in)))));
+else % DO NOT Normalize to NRB -- (Output = sqrt(CARS)*exp(j*phi))
+    temp = real(sqrt(CARS)).*exp(j.*PhaseOffset+j.*imag(Hilbert(.5.*real(log(temp_in)))));
+end
+
+KK_Imag = imag(temp);
+KK_Real = real(temp);
